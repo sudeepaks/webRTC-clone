6613d7e60e95b50077cca9236a974d0322713ef0
sudeepaks <92206271+sudeepaks@users.noreply.github.com>
2024-07-17 13:18:20 +0530

    add

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from './components/Landing';
+import { Room } from './components/Room';
+
+
+function App() {
+ 
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/room" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+     
+    
+  )
+}
+
+export default App
